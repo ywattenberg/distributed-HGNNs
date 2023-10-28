a12f9a52b162824d2aa3c5cbc7a89b5ee2733ac0
grker <stef.kramer@bluewin.ch>
2023-10-28 15:21:29 +0200

    temporary commit

--- main.cpp
+++ main.cpp
@@ -6,15 +6,43 @@
 
 int main() {
   torch::Tensor tensor = torch::rand({2, 3});
-  std::cout << &tensor << std::endl;
-  std::cout << tensor << std::endl;
+  std::vector<float> customData = {0.0, 0.0, 0.0, 4.0, 0.0, 0.0};
+  std::vector<int64_t> shape = {3,2};
+  tensor = tensor.to_sparse();
+
+  at::Tensor customTensor = torch::from_blob(customData.data(), shape);
+  customTensor = customTensor.to_sparse();
+
+  at::Tensor res2 = tensor.mm(customTensor);
+  at::Tensor res = torch::mm(tensor, customTensor);
+
+  std::cout << "tensor:\n" << tensor << std::endl;
+  std::cout << "sparse:\n" << customTensor << std::endl;
+  auto indices = customTensor.indices();
+  std::cout << "indices:\n" << indices << std::endl;
+
+  std::cout << "Res 2:\n" << res2 << std::endl;
+  std::cout << "Res:\n" << res << std::endl;
+
 
   std::vector<int> layers;
-  layers.push_back(4);
-  layers.push_back(19);
+  layers.push_back(5);
+  layers.push_back(5);
 
   std::cout << layers[0] << std::endl;
 
-  Model *m = new Model(10, layers, 5, 0.2, false);
+  torch::Tensor input = torch::rand({5,5});
+  Model* m = new Model(5, layers, 5, 0.0, false);
+
+  std::cout << "input: " << input << std::endl;
+
+  at::Tensor identityMatrix = torch::eye(5);
+  std::cout << "output: " << m->forward(input, identityMatrix, true) << std::endl;
+  
+  std::cout << "input: " << input << std::endl;
+
+
    
 }
+
+
