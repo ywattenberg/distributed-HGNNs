57a690abbc6bd121666f8b94309977368b14ee28
Kreuzes <oxotoxigon@gmail.com>
2023-11-02 18:07:10 +0100

    temporary push, working yaml reader, small bug in conversion

--- model/model.h
+++ model/model.h
@@ -6,43 +6,40 @@
 #include <iostream>
 
 
-struct HGNN_conv : torch::nn::Module
-{
-    private: 
-        torch::Tensor weights;
-        torch::Tensor bias;
-
-    public:
-        HGNN_conv(int in_dim, int out_dim, bool withBias);
-
-        // TODO: add Graph as parameter
-        torch::Tensor forward(torch::Tensor &input, torch::Tensor &leftSide);
-};
-
-struct Model : torch::nn::Module
+class HGNN_conv : public torch::nn::Module
 {
     private:
-        // TODO: Graph class
+        torch::nn::Linear linear_layer = nullptr;
+
+    public:
+        HGNN_conv(int in_dim, int out_dim, bool withBias, bool t);
+        torch::Tensor forward(const torch::Tensor &input);
+
+};
+
+class Model : public torch::nn::Module
+{
+    private:
         int input_dim;
         std::vector<int>* layer_dim;
         int output_dim;
         int number_of_hid_layers;
-        double dropout;
         std::vector<std::shared_ptr<HGNN_conv>> layers;
+        double dropout;
+        const torch::Tensor *leftSide;
 
     public:
-
-        Model(int in_dim, std::vector<int> &lay_dim, int out_dim, double dropout, bool withBias);
+        Model(int in_dim, std::vector<int> lay_dim, int out_dim, double dropout, const torch::Tensor *leftSide, bool withBias);
 
         // forward function of the Model, it takes the features X (called input) and the constant leftSide of the expression 10 of the paper 
         // Hypergraph Neural Networks (called leftSide)
-        torch::Tensor forward(torch::Tensor &input, torch::Tensor &leftSide, bool train);
+        torch::Tensor forward(const torch::Tensor &input);
 
 
 };
 
 
-struct HGNN_fc : torch::nn::Module
+class HGNN_fc : torch::nn::Module
 {
     private:
         torch::Tensor* weights;
