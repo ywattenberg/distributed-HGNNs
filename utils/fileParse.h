dc95156a68dbe521fcb788514c38e3e2d6d7caf7
grker <stef.kramer@bluewin.ch>
2023-11-05 22:02:19 +0100

    handling wrong types in parsing process

--- utils/fileParse.h
+++ utils/fileParse.h
@@ -20,23 +20,35 @@ std::tuple<int,int> csvToArray(std::string &&filePath, std::vector<T> &parsedCsv
     std::fstream data(filePath, std::fstream::in);
     std::string line;
     int lines = 0;
+    int illegal_elements = 0;
     while(std::getline(data,line))
-    {
+    {        
         ++lines;
         std::stringstream lineStream(line);
         std::string cell;
         std::vector<std::string> parsedRow;
+
         while(std::getline(lineStream,cell,','))
         {
             parsedRow.push_back(cell);
-            if(std::is_same<T, float>::value)
-                parsedCsv.push_back(std::stof(cell));
-            else if (std::is_same<T, double>::value)
-                parsedCsv.push_back(std::stod(cell));
-            else if (std::is_same<T, int>::value)
-                parsedCsv.push_back(std::stoi(cell));
+
+            try {
+                if(std::is_same<T, float>::value)
+                    parsedCsv.push_back(std::stof(cell));
+                else if (std::is_same<T, double>::value)
+                    parsedCsv.push_back(std::stod(cell));
+                else if (std::is_same<T, int>::value)
+                    parsedCsv.push_back(std::stoi(cell));
+
+            } catch(std::invalid_argument&) {
+                // non parsable row
+                std::cout << "Element in row was not parseable" << std::endl;
+                while (std::getline(lineStream,cell,','));
+                lines--;
+            }
         }
     }
-    return std::make_tuple(lines, parsedCsv.size()/lines);
+
+    return std::make_tuple(lines, (parsedCsv.size())/lines);
 }
 #endif
