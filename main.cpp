77aa28c8aca52527b7ff5d4825b0da890787f754
Yannick Wattenberg <wattenberg@ywattenberg.com>
2023-11-09 16:10:17 +0100

    create new configParse and move yaml logic there further make path to config a cmd line option

--- main.cpp
+++ main.cpp
@@ -1,43 +1,40 @@
-#include <torch/torch.h>
 #include <iostream>
-#include <yaml-cpp/yaml.h>
 #include <filesystem>
+#include <unistd.h>
 
-#include "utils/fileParse.h"
+#include <yaml-cpp/yaml.h>
+#include <torch/torch.h>
+
 #include "model/model.h"
 #include "trainer/trainer.h"
+#include "utils/fileParse.h"
+#include "utils/configParse.h"
+
 
 
 using LossFunction = at::Tensor(*)(const at::Tensor&, const at::Tensor&); //Supertype for loss functions
 
 
-int main(){
+int main(int argc, char** argv){
+  // read command line arguments
+  int opt;
+  std::string config_path;
+  while((opt = getopt(argc, argv, "c:")) != -1){
+    switch(opt){
+      case 'c':
+        config_path = optarg;
+        break;
+      default:
+        std::cerr << "Usage: " << argv[0] << " -c <config_path>" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+  }
+
   // load config
-  std::string config_path = std::string(std::filesystem::current_path()) + "/../config/test.yaml";
-  YAML::Node config = YAML::LoadFile(config_path);
-  std::cout << "Config: " << config << std::endl;
-
-  // load parameters
-  YAML::Node model_conf = config["model"];
-  YAML::Node trainer_conf = config["trainer"];
-  YAML::Node data_config = config["data"];
-
-  std::vector<int> HIDDEN_DIMS =  model_conf["hidden_dims"].as<std::vector<int>>();
-  double DROPOUT =  model_conf["dropout_rate"].as<double>();
-  bool WITH_BIAS =  model_conf["with_bias"].as<bool>();
-
-  double LEARNING_RATE = trainer_conf["learning_rate"].as<double>();
-  int EPOCHS = trainer_conf["epochs"].as<int>();
-  int OUTPUT_STEPSIZE = trainer_conf["output_stepsize"].as<int>(); //interval of epochs to output the loss
-  
-  int CLASSES = model_conf["classes"].as<int>();
-  
-  std::string G_path = data_config["G_path"].as<std::string>();
-  std::string Labels_path = data_config["labels_path"].as<std::string>();
-  std::string Features_path = data_config["features_path"].as<std::string>();
+  ConfigProperties config = ParseConfig(config_path);
 
   std::vector<float> data;
-  auto [G_lines, G_cols] = csvToArray(std::move(G_path), data);
+  auto [G_lines, G_cols] = csvToArray(std::move(config.G_path), data);
   torch::Tensor leftSide = torch::from_blob(data.data(), {G_lines,G_cols});
   std::cout << "G dimensions: " << G_lines << "x" << G_cols << std::endl;
   
@@ -51,23 +48,23 @@ int main(){
   std::cout << "leftSide dimensions: " << leftSide.sizes() << std::endl;
 
   std::vector<float> data2;
-  auto [L_lines, L_cols] = csvToArray(std::move(Labels_path), data2);
+  auto [L_lines, L_cols] = csvToArray(std::move(config.labels_path), data2);
   torch::Tensor labels = torch::from_blob(data2.data(), {L_lines,L_cols});
   labels = labels.index({at::indexing::Slice(), at::indexing::Slice(1)}).squeeze().to(torch::kLong);
   std::cout << "labels shape: " << labels.sizes() << std::endl;
  
   std::vector<int> data3; 
-  auto [F_lines, F_cols] = csvToArray(std::move(Features_path), data3);
+  auto [F_lines, F_cols] = csvToArray(std::move(config.features_path), data3);
   torch::Tensor features = torch::from_blob(data3.data(), {F_lines,F_cols});
   std::cout << "features shape: " << features.sizes() << std::endl;
 
   // Build Model
-  auto model = new Model(F_cols, HIDDEN_DIMS, CLASSES, DROPOUT, &leftSide, WITH_BIAS);
+  auto model = new Model(F_cols, config.hidden_dims, config.classes, config.dropout_rate, &leftSide, config.with_bias);
 
   // Define the loss function
   LossFunction ce_loss_fn = [](const torch::Tensor& predicted, const torch::Tensor& target) {
         return torch::nn::functional::cross_entropy(predicted, target);
     };
   // Train the model
-  train_model(EPOCHS, OUTPUT_STEPSIZE, labels, features, ce_loss_fn, model, LEARNING_RATE);
+  train_model(config.epochs, config.output_stepsize, labels, features, ce_loss_fn, model, config.learning_rate);
 }
