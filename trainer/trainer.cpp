61826000f54a297200df74c0d8eb1edebe574d38
Yannick Wattenberg <wattenberg@ywattenberg.com>
2023-11-13 12:54:55 +0100

    change training to only train on trainset

--- trainer/trainer.cpp
+++ trainer/trainer.cpp
@@ -1,7 +1,11 @@
 #include <torch/torch.h>
 #include <vector>
 #include <iostream>
+#include "trainer.h"
+
+
 #include "../model/model.h"
+#include "../utils/scores.h"
 #include "../utils/configParse.h"
 
 
@@ -15,21 +19,37 @@ void train_model(const ConfigProperties& config, torch::Tensor &labels, torch::T
     int stepsize_output = config.output_stepsize;
     long train_set_cutoff = config.test_idx;
 
+    torch::Tensor train_labels = labels.index({at::indexing::Slice(0,train_set_cutoff)});
+    torch::Tensor test_labels = labels.index({at::indexing::Slice(train_set_cutoff,labels.size(0))});
+
     torch::optim::Adam optimizer(model->parameters(), torch::optim::AdamOptions(lr));
     // torch::optim::SGD optimizer(model.parameters(), torch::optim::SGDOptions(lr));
 
     for (int epoch = 0; epoch < n_epochs; epoch++){
         torch::Tensor predictions = model->forward(input_features);
-        torch::Tensor loss = loss_fn(predictions, labels);
+        torch::Tensor train_predictions = predictions.index({at::indexing::Slice(0,train_set_cutoff)});
+        torch::Tensor loss = loss_fn(train_predictions, train_labels);
 
         optimizer.zero_grad();
         loss.backward();
         optimizer.step();
 
         if (epoch % stepsize_output == 0){
-            std::cout << "Epoch [" << epoch << "/" << n_epochs << "], Loss: " << loss.item<double>() 
-            // << ", Predictions: " << round(predictions,2)
-            << std::endl;
+            {
+                torch::NoGradGuard no_grad;
+                torch::Tensor test_predictions = predictions.index({at::indexing::Slice(train_set_cutoff,labels.size(0))});
+                torch::Tensor test_loss = loss_fn(test_predictions, test_labels);
+                torch::Tensor acc = accuracy(test_predictions, test_labels);
+                torch::Tensor f1 = f1_score(test_predictions, test_labels);
+
+                std::cout << "Epoch [" << epoch << "/" << n_epochs << "], ";
+                std::cout << "Train Loss: " << loss.item<double>() << ", ";
+                std::cout << "Test Loss: " << test_loss.item<double>() << ", ";
+                std::cout << "Test Accuracy: " << acc.item<double>() << ", ";
+                std::cout << "Test F1: " << f1.item<double>() << std::endl;
+                // << ", Predictions: " << round(predictions,2)
+            }
+            
         }
     }
 }
