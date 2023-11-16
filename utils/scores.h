2244c93a73feb1e69f5c68217d08ea14c037ec3d
grker <stef.kramer@bluewin.ch>
2023-11-16 16:32:30 +0100

    fixed f1-score

--- utils/scores.h
+++ utils/scores.h
@@ -5,8 +5,8 @@
 #include <torch/torch.h>
 
 torch::Tensor accuracy(torch::Tensor &y_pred, torch::Tensor &y_true);
-torch::Tensor precision(torch::Tensor &y_pred, torch::Tensor &y_true);
-torch::Tensor recall(torch::Tensor &y_pred, torch::Tensor &y_true);
-torch::Tensor f1_score(torch::Tensor &y_pred, torch::Tensor &y_true);
+torch::Tensor precision(torch::Tensor &y_pred, torch::Tensor &y_true, int label);
+torch::Tensor recall(torch::Tensor &y_pred, torch::Tensor &y_true, int label);
+torch::Tensor f1_score(torch::Tensor &y_pred, torch::Tensor &y_true, int num_classes);
 
 #endif 
