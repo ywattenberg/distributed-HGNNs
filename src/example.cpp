f48aead3b8c516c7921d5060eed22196d8145dca
grker <stef.kramer@bluewin.ch>
2023-11-23 15:33:11 +0100

    working example

--- src/example.cpp
+++ src/example.cpp
@@ -17,18 +17,72 @@ extern "C" {
 }
 #define N 5
 
-int main()
+using namespace std;
+using namespace combblas;
+
+#define ITERATIONS 10
+#define EDGEFACTOR 8
+
+int main(int argc, char* argv[])
 {
-	MPI_Init(NULL, NULL);
+	// MPI_Init(NULL, NULL);
+
+	// int nprocs, myrank;
+	// MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
+	// MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
+
+	// std::cout << "Hello from process " << myrank << " of " << nprocs << std::endl;
+	// AWeighted = new SpParMat<int64_t, double, SpDCCols<int64_t, double>>(MPI_COMM_WORLD);
+	 
+	// CombBLAS::SpParMat<int, double, CombBLAS::SpDCCols<int,double> > A;
+
+
 
 	int nprocs, myrank;
+	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
 	MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
 
-	std::cout << "Hello from process " << myrank << " of " << nprocs << std::endl;
-	 AWeighted = new SpParMat<int64_t, double, SpDCCols<int64_t, double>>(MPI_COMM_WORLD);
-	 
-	CombBLAS::SpParMat<int, double, SpDCCols<int,double> > A;
+	if(argc < 2)
+	{
+		if(myrank == 0)
+		{
+			cout << "Usage: ./IndexingTiming <Scale>" << endl;
+		}
+		MPI_Finalize(); 
+		return -1;
+	}				
+
+		cout << "i have rank " << myrank << endl;
+		typedef SpParMat <int, double, SpDCCols<int,double> > PARDBMAT;
+		PARDBMAT *A, *B;		// declare objects
+ 		double initiator[4] = {.6, .4/3, .4/3, .4/3};
+		DistEdgeList<int64_t> * DEL = new DistEdgeList<int64_t>();
+
+		int scale = static_cast<unsigned>(atoi(argv[1]));
+		ostringstream outs, outs2, outs3;
+		outs << "Forcing scale to : " << scale << endl;
+		SpParHelper::Print(outs.str());
+		DEL->GenGraph500Data(initiator, scale, EDGEFACTOR, true, true );        // generate packed edges
+		SpParHelper::Print("Generated renamed edge lists\n");
+		
+		// conversion from distributed edge list, keeps self-loops, sums duplicates
+		A = new PARDBMAT(*DEL, false); 	// already creates renumbered vertices (hence balanced)
+		delete DEL;	// free memory before symmetricizing
+		SpParHelper::Print("Created double Sparse Matrix\n");
+
+		A->PrintInfo();
+		// auto tmp = A->spSeq;
+		// cout << tmp << endl;
+
+		// PARDBMAT::Iterator it = A.begnz();
+
+		// while (it != A.endnz()) {
+    // int row_index = it.row();
+    // int col_index = it.col();
+    // double value = *it;
+	
+
 
 	// int n, inca = 1, incb = 1, i;
 	// std::complex<double> a[N], b[N], c;
