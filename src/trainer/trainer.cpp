d6b298813304e8e6ece9840fa8111b797c109794
niedo <siwachte@ethz.ch>
2023-12-18 15:54:49 +0100

    changes to do timed runs

--- src/trainer/trainer.cpp
+++ src/trainer/trainer.cpp
@@ -1,6 +1,7 @@
 #include <torch/torch.h>
 #include <vector>
 #include <iostream>
+#include <fstream>
 #include "trainer.h"
 
 
@@ -12,7 +13,12 @@
 using LossFunction = at::Tensor(*)(const at::Tensor&, const at::Tensor&);
 
 
-void train_model(const ConfigProperties& config, torch::Tensor &labels, torch::Tensor &input_features, LossFunction loss_fn, BaseModel *model){
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::milliseconds;
+
+void train_model(const ConfigProperties& config, torch::Tensor &labels, torch::Tensor &input_features, LossFunction loss_fn, BaseModel *model, int run_id, bool timing, std::string timing_file){
     
     double lr = config.trainer_properties.learning_rate;
     int n_epochs = config.trainer_properties.epochs;
@@ -28,8 +34,10 @@ void train_model(const ConfigProperties& config, torch::Tensor &labels, torch::T
     torch::optim::Adam optimizer(model->parameters(), torch::optim::AdamOptions(lr).weight_decay(0.0005));
 
     torch::optim::StepLR lr_scheduler = torch::optim::StepLR(optimizer, lr_step_size, lr_gamma);
-    
+     
+    auto t1 = high_resolution_clock::now();
     for (int epoch = 0; epoch < n_epochs; epoch++){
+
         torch::Tensor predictions = model->forward(input_features);
         torch::Tensor train_predictions = predictions.index({at::indexing::Slice(0,train_set_cutoff)});
         torch::Tensor loss = loss_fn(train_predictions, train_labels);
@@ -41,18 +49,33 @@ void train_model(const ConfigProperties& config, torch::Tensor &labels, torch::T
 
         if (epoch % stepsize_output == 0){
             {
+
+                auto t2 = high_resolution_clock::now();
+                /* Getting number of milliseconds as an integer. */
+                auto ms_int = duration_cast<milliseconds>(t2 - t1);
+                /* Getting number of milliseconds as a double. */
+                duration<double, std::milli> ms_double = t2 - t1;
+
                 torch::NoGradGuard no_grad;
                 torch::Tensor test_predictions = predictions.index({at::indexing::Slice(train_set_cutoff,labels.size(0))});
                 torch::Tensor test_loss = loss_fn(test_predictions, test_labels);
                 torch::Tensor acc = accuracy(test_predictions, test_labels);
                 torch::Tensor f1 = f1_score(test_predictions, test_labels, 40);
 
-                std::cout << "Epoch [" << epoch << "/" << n_epochs << "], ";
+                std::cout << "Epoch [" << epoch << "/" << n_epochs << "], " << ms_int.count() << "ms, ";
                 std::cout << "Train Loss: " << loss.item<double>() << ", ";
                 std::cout << "Test Loss: " << test_loss.item<double>() << ", ";
                 std::cout << "Test Accuracy: " << acc.item<double>() << ", ";
                 std::cout << "Test F1: " << f1.item<double>() << std::endl;
 
+                if (timing){
+                    std::ofstream outfile;
+                    outfile.open(timing_file, std::ios_base::app);
+                    // append time to csv file
+                    outfile << run_id << "," << epoch << "," << ms_int.count() << "," << loss.item<double>() << "," << test_loss.item<double>() << "," << acc.item<double>() << "," << f1.item<double>() << "\n";
+                    outfile.close();
+                }
+                t1 = high_resolution_clock::now();
                 // << ", Predictions: " << round(predictions,2)
             }
             
