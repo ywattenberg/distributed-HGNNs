de53dadeae36008d2909150c75118e439d968adc
niedo <siwachte@ethz.ch>
2023-12-28 14:22:59 +0100

    changed scripts according to new repository name

--- scripts/base.sh
+++ scripts/base.sh
@@ -2,8 +2,8 @@
 #SBATCH --mail-type=NONE
 #SBATCH --job-name=thnn
 #SBATCH --time=00:30:00
-#SBATCH --output=/cluster/home/%u/distributed-THNN/log/%j.out
-#SBATCH --error=/cluster/home/%u/distributed-THNN/log/%j.err
+#SBATCH --output=/cluster/home/%u/distributed-HGNNs/log/%j.out
+#SBATCH --error=/cluster/home/%u/distributed-HGNNs/log/%j.err
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=9
@@ -35,7 +35,7 @@ echo "SLURM_JOB_NODELIST:    ${SLURM_JOB_NODELIST}"
 echo "SLURM_NTASKS:     ${SLURM_NTASKS}"
 echo "CPUS:                 ${CPUS}"
 
-rsync -ah --stats /cluster/home/$USER/distributed-THNN/data $TMPDIR
+rsync -ah --stats /cluster/home/$USER/distributed-HGNNs/data $TMPDIR
 # rsync -ah --stats /cluster/scratch/$USER/data/random $TMPDIR/data
 
 # echo "Data copied at:     $(date)"
@@ -46,7 +46,7 @@ module load gcc/11.4.0 openmpi openblas cmake/3.26.3 eth_proxy curl
 
 echo "Dependencies installed"
 
-cd $HOME/distributed-THNN/build
+cd $HOME/distributed-HGNNs/build
 cmake ..
 make -j $CPUS
 
@@ -56,13 +56,13 @@ echo "Starting training at:     $(date)"
 
 # bash $HOME/discord-webhook/discord.sh --webhook-url=https://discord.com/api/webhooks/1105789194959339611/-tDqh7eGfQJhaLoxjCsHbHrwTzhNEsR5SDxabXFiYdhg-KHwzN3kVwr87rxUggqWCQ0K --title "Starting training for $USER" --color 3066993 --field "Date;$(date);false" --field "Jobid;${SLURM_JOB_ID};false"
 
-# $HOME/distributed-THNN/build/torchtest -c "${HOME}/distributed-THNN/config/paper_config.yaml" -u ${USER} -t ${TMPDIR}
-# mpiexec -np ${SLURM_NTASKS} $HOME/distributed-THNN/build/example_2 ${TMPDIR}/data/m_g_ms_gs/DVH.mtx ${TMPDIR}/data/m_g_ms_gs/invDE_HT_DVH.mtx ${TMPDIR}/data/m_g_ms_gs/DVH_times_invDE_HT_DVH.mtx
-# mpiexec -np ${SLURM_NTASKS}  $HOME/distributed-THNN/build/openBLAS_example
-# mpiexec -np ${SLURM_NTASKS}  $HOME/distributed-THNN/build/timing
-# mpiexec -np ${SLURM_NTASKS}  $HOME/distributed-THNN/build/model ${HOME}/distributed-THNN/config/paper_config.yaml
-# mpiexec -np ${SLURM_NTASKS} $HOME/distributed-THNN/build/matrix_bench -t "/cluster/scratch/siwachte" -i ${SLURM_JOB_ID} -p $CPUS
-mpiexec -np ${SLURM_NTASKS} $HOME/distributed-THNN/build/spmatrix_bench -t "/cluster/scratch/siwachte" -i ${SLURM_JOB_ID} -p $CPUS
+# $HOME/distributed-HGNNs/build/torchtest -c "${HOME}/distributed-HGNNs/config/paper_config.yaml" -u ${USER} -t ${TMPDIR}
+# mpiexec -np ${SLURM_NTASKS} $HOME/distributed-HGNNs/build/example_2 ${TMPDIR}/data/m_g_ms_gs/DVH.mtx ${TMPDIR}/data/m_g_ms_gs/invDE_HT_DVH.mtx ${TMPDIR}/data/m_g_ms_gs/DVH_times_invDE_HT_DVH.mtx
+# mpiexec -np ${SLURM_NTASKS}  $HOME/distributed-HGNNs/build/openBLAS_example
+# mpiexec -np ${SLURM_NTASKS}  $HOME/distributed-HGNNs/build/timing
+# mpiexec -np ${SLURM_NTASKS}  $HOME/distributed-HGNNs/build/model ${HOME}/distributed-HGNNs/config/paper_config.yaml
+# mpiexec -np ${SLURM_NTASKS} $HOME/distributed-HGNNs/build/matrix_bench -t "/cluster/scratch/siwachte" -i ${SLURM_JOB_ID} -p $CPUS
+mpiexec -np ${SLURM_NTASKS} $HOME/distributed-HGNNs/build/spmatrix_bench -t "/cluster/scratch/siwachte" -i ${SLURM_JOB_ID} -p $CPUS
 
 echo "Finished training at:     $(date)"
 
--- scripts/forward-timing.sh
+++ scripts/forward-timing.sh
@@ -2,8 +2,8 @@
 #SBATCH --mail-type=NONE
 #SBATCH --job-name=fwd-timing
 #SBATCH --time=01:00:00
-#SBATCH --output=/cluster/home/%u/distributed-THNN/log/%j.out
-#SBATCH --error=/cluster/home/%u/distributed-THNN/log/%j.err
+#SBATCH --output=/cluster/home/%u/distributed-HGNNs/log/%j.out
+#SBATCH --error=/cluster/home/%u/distributed-HGNNs/log/%j.err
 #SBATCH --ntasks=16
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
@@ -33,7 +33,7 @@ echo "SLURM_JOB_ID:    ${SLURM_JOB_ID}"
 echo "SLURM_JOB_NODELIST:    ${SLURM_JOB_NODELIST}"
 echo "SLURM_NTASKS:    ${SLURM_NTASKS}"
 
-rsync -ah --stats /cluster/home/$USER/distributed-THNN/data $TMPDIR
+rsync -ah --stats /cluster/home/$USER/distributed-HGNNs/data $TMPDIR
 rsync -ah --stats /cluster/scratch/$USER/data/random $TMPDIR/data
 ls $TMPDIR/data
 ls $TMPDIR/data/random
@@ -45,7 +45,7 @@ module load gcc/11.4.0 openmpi openblas cmake/3.26.3 eth_proxy curl
 
 echo "Dependencies installed"
 
-cd $HOME/distributed-THNN/build
+cd $HOME/distributed-HGNNs/build
 cmake ..
 make -j ${SLURM_NTASKS}
 
@@ -55,7 +55,7 @@ echo "Starting timing run at:     $(date)"
 
 # bash $HOME/discord-webhook/discord.sh --webhook-url=https://discord.com/api/webhooks/1105789194959339611/-tDqh7eGfQJhaLoxjCsHbHrwTzhNEsR5SDxabXFiYdhg-KHwzN3kVwr87rxUggqWCQ0K --title "Starting timing for $USER" --color 3066993 --field "Date;$(date);false" --field "Jobid;${SLURM_JOB_ID};false"
 
-$HOME/distributed-THNN/build/forward_timing -c ${HOME}/distributed-THNN/config/forward_timing.yaml -t $TMPDIR -i ${SLURM_JOB_ID} -p ${SLURM_NTASKS} -d 1
+$HOME/distributed-HGNNs/build/forward_timing -c ${HOME}/distributed-HGNNs/config/forward_timing.yaml -t $TMPDIR -i ${SLURM_JOB_ID} -p ${SLURM_NTASKS} -d 1
 
 echo "Finished timing run at:     $(date)"
 
