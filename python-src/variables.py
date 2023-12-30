0a41066f3f70ac542d8cefe847da6fe070074ced
Kreuzes <oxotoxigon@gmail.com>
2023-12-30 15:52:04 +0100

    add definition for gplus num classes

--- python-src/variables.py
+++ python-src/variables.py
@@ -3,6 +3,7 @@ import torch
 DEFAULT_SCRIPT_PATH = "../config/paper_config.yaml"
 DEFAULT_DATA_PATH = "../data"
 MODELNET40_NUM_CLASSES = 40
+GPLUS_NUM_CLASSES = 469 # 468 + 1 -> extra class for isolated vertices
 
 FEATURE_TYPE = torch.float
 LABEL_TYPE = torch.long
